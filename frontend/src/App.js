2ba5479b918ad75e9fa251e6cc08d069068290ea
Pedro Sousa <ppls2106@gmail.com>
2020-03-29 10:02:22 -0300

    estado e imutabilidade

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './Header'
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
   return (
-    <Header>
-      Semana OmniStack 11
-    </Header>
+    <div>
+      <Header>
+        Semana OmniStack 11<br />
+        contador: {counter}
+      </Header>
+      <button onClick={increment}>Increment</button>
+    </div>
   );
 }
 
